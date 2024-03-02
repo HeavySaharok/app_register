db26367dc68b0130c6a79c83d8d2b6bd8a5c33e3
HeavySaharok <daniil.voytik@gmail.com>
2024-03-02 13:46:43 +0300

    журнал работ

---
+++ work_log.py
@@ -0,0 +1,58 @@
+import datetime
+
+from flask import Flask, render_template, redirect
+
+from data.jobs import Jobs
+from forms.user import RegisterForm
+from data.news import News
+from data.users import User
+from data import db_session
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    db_session.global_init("db/mars2.db")
+    app.run()
+
+
+@app.route("/")
+def index():
+    session = db_session.create_session()
+    jobs = session.query(Jobs).all()
+    users = session.query(User).all()
+    names = {name.id: (name.surname, name.name) for name in users}
+    return render_template("index.html", jobs=jobs, names=names)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация', form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация', form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            surname=form.surname.data,
+            age = form.age.data,
+            position = form.position.data,
+            speciality = form.speciality.data,
+            address = form.address.data,
+            hashed_password = form.set_password(form.password.data)
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+if __name__ == '__main__':
+    main()
