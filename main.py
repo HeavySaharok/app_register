866b084873d891dc2029366e80e8aa8b1d8d11ab
HeavySaharok <daniil.voytik@gmail.com>
2024-03-05 18:48:27 +0300

    РАБОТАЕЕЕЕТ

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, abort
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
+from data.jobs import Jobs
+from forms.jobs import JobsForm
+from forms.user import RegisterForm, LoginForm
 from forms.user import RegisterForm
 from data.news import News
 from data.users import User
 from data import db_session
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 def main():
     db_session.global_init("db/mars.db")
-    app.run()
+    app.run(debug=True)
 
 
 @app.route("/")
 def index():
-    db_sess = db_session.create_session()
-    news = db_sess.query(News).filter(News.is_private != True)
-    return render_template("index.html", news=news)
+    session = db_session.create_session()
+    jobs = session.query(Jobs).all()
+    users = session.query(User).all()
+    names = {name.id: (name.surname, name.name) for name in users}
+    return render_template("index.html", jobs=jobs, names=names)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -36,17 +57,30 @@ def reqister():
             name=form.name.data,
             email=form.email.data,
             surname=form.surname.data,
-            age = form.age.data,
-            position = form.position.data,
-            speciality = form.speciality.data,
-            address = form.address.data,
-            hashed_password = form.set_password(form.password.data)
+            age=form.age.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address=form.address.data,
+            hashed_password=form.set_password(form.password.data)
         )
         user.set_password(form.password.data)
         db_sess.add(user)
         db_sess.commit()
         return redirect('/')
     return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)  # тыц, там session
+            return redirect("/")
+        return render_template('login.html', message="Неправильный логин или пароль", form=form)
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 if __name__ == '__main__':
